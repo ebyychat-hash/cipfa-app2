70f4aa3a3f604cc2824dc8719ac49dc99d376702
ebyychat-hash <ebyychat@gmail.com>
2025-09-03 16:17:09 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'cipfa-pwa-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.method === 'GET' && url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(cached =>
+        cached || fetch(e.request).then(resp => {
+          const copy = resp.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+          return resp;
+        })
+      )
+    );
+  }
+});
+
+// Allow page to request a notification via postMessage
+self.addEventListener('message', (e) => {
+  if (e.data && e.data.type === 'notify' && self.registration && self.registration.showNotification) {
+    const { title, body } = e.data;
+    self.registration.showNotification(title, { body });
+  }
+});
